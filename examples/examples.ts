c61c2fadfb251824e3731bede3c7db76883a21ed
Cubiquitous <claudiov.dejesus@protonmail.com>
2024-02-04 10:20:44 -0300

    clean up

--- examples/examples.ts
+++ examples/examples.ts
@@ -18,31 +18,35 @@ const {
 
 const playerId = Number(USER_ID__USERID);
 const serverId = Number(SERVER_ID);
+const steamId = Number(STEAM_ID__STEAMID);
 
 const bm = new BattleMetrics(TOKEN);
 
 async function main(): Promise<void> {
   // const search = await bm.player.search({ filterOnline: false });
   // print(search);
+  //
   // const info = await bm.player.info(playerId);
   // print(info);
+  //
   // const playHistory = await bm.player.playHistory({ playerId, serverId });
   // print(playHistory);
+  //
   // const serverInfo = await bm.player.serverInfo(playerId, serverId);
   // print(serverInfo);
-  // try {
-  //   const matchIdentifiers = await bm.player.matchIdentifiers(
-  //     "76561198151275725",
-  //     "steamID"
-  //   );
-  // } catch (e) {
-  //   console.log(e);
-  // }
+  //
+  //const matchIdentifiers = await bm.player.matchIdentifiers(
+  //   "76561198151275725",
+  //   "steamID"
+  //);
   // print(matchIdentifiers);
+  //
   // const sessionHistory = await bm.player.sessionHistory(playerId);
   // print(sessionHistory);
+  //
   // const addFlag = await bm.player.addFlag(playerId);
   // print(addFlag);
+  //
   // const flags = await bm.player.flags(playerId);
   // print(flags);
 }
