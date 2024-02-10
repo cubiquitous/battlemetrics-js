177d525c22b7f5bae7e89db74a89f7a5e7969d8a
Cubiquitous <claudiov.dejesus@protonmail.com>
2024-02-10 09:41:34 -0300

    add new classes

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,13 @@
+import { BanList } from "./components/banlist.ts";
+import { Bans } from "./components/bans.ts";
 import { Flags } from "./components/flags.ts";
 import Helpers from "./components/helpers.js";
+import { Notes } from "./components/notes.ts";
 import Player from "./components/players.js";
+import { Session } from "./components/session.ts";
+import { GameInfo } from "./components/gameInfo.ts";
+import { Organization } from "./components/organization.ts";
+import { Server } from "./components/server.ts";
 
 export default class BattleMetrics {
   private baseURL: string;
@@ -10,6 +17,13 @@ export default class BattleMetrics {
   private helpers: Helpers;
   public player: Player;
   public flags: Flags;
+  public notes: Notes;
+  public server: Server;
+  public session: Session;
+  public banList: BanList;
+  public bans: Bans;
+  public organization: Organization;
+  public gameInfo: GameInfo;
 
   public constructor(token: string | undefined) {
     if (typeof token !== "string") {
@@ -23,7 +37,10 @@ export default class BattleMetrics {
     this.helpers = new Helpers(this.headers, this.baseURL);
     this.player = new Player(this.helpers, this.baseURL);
     this.flags = new Flags(this.helpers, token);
-  }
-}
-
-// console.dir(myObject, { depth: null });
+    this.server = new Server(this.helpers);
+    this.notes = new Notes(this.helpers);
+    this.session = new Session(this.helpers);
+    this.banList = new BanList(this.helpers);
+    this.bans = new Bans(this.helpers);
+    this.organization = new Organization(this.helpers);
+    this.gameInfo = new GameInfo(this.helpers);
