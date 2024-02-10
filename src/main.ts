d8fd74be4085982743bf1b6a056c56540d4f244d
Cubiquitous <claudiov.dejesus@protonmail.com>
2024-02-10 09:42:15 -0300

    add methods to main.ts

--- src/main.ts
+++ src/main.ts
@@ -35,7 +35,7 @@ export default class BattleMetrics {
     this.headers = { Authorization: `Bearer ${this.apiKey}` };
     this.responseData = null;
     this.helpers = new Helpers(this.headers, this.baseURL);
-    this.player = new Player(this.helpers, this.baseURL);
+    this.player = new Player(this.helpers);
     this.flags = new Flags(this.helpers, token);
     this.server = new Server(this.helpers);
     this.notes = new Notes(this.helpers);
@@ -44,3 +44,94 @@ export default class BattleMetrics {
     this.bans = new Bans(this.helpers);
     this.organization = new Organization(this.helpers);
     this.gameInfo = new GameInfo(this.helpers);
+  }
+
+  async checkApiScopes(token?: string): Promise<object> {
+    if (!token) {
+      token = this.apiKey;
+    }
+
+    return await this.helpers.makeRequest({
+      method: "POST",
+      path: "",
+      data: JSON.stringify({ token }),
+    });
+  }
+
+  async pagination(pageLink: string): Promise<object> {
+    return await this.helpers.makeRequest({
+      method: "GET",
+      path: pageLink,
+    });
+  }
+
+  async metrics(
+    name: string = "games.rust.players",
+    startDate?: string,
+    endDate?: string,
+    resolution: string = "60"
+  ): Promise<object> {
+    if (!startDate) {
+      const now = new Date();
+      startDate = new Date(
+        now.getTime() - 1 * 24 * 60 * 60 * 1000
+      ).toISOString(); // 1 day ago
+    }
+
+    if (!endDate) {
+      endDate = new Date().toISOString();
+    }
+
+    const data = {
+      "metrics[0][name]": name,
+      "metrics[0][range]": `${startDate}:${endDate}`,
+      "metrics[0][resolution]": resolution,
+      "fields[dataPoint]": "name,group,timestamp,value",
+    };
+
+    return await this.helpers.makeRequest({
+      method: "GET",
+      path: "/metrics",
+      params: new URLSearchParams(data),
+    });
+  }
+
+  async activityLogs(
+    filterBmid?: number,
+    filterSearch?: string,
+    filterServers?: number,
+    blacklist?: string,
+    whitelist?: string
+  ): Promise<object> {
+    const data: { [key: string]: string | number } = {
+      "page[size]": "100",
+      include: "organization,server,user,player",
+    };
+
+    if (blacklist) {
+      data["filter[types][blacklist]"] = blacklist;
+    }
+
+    if (whitelist) {
+      data["filter[types][whitelist]"] = whitelist;
+    }
+
+    if (filterServers) {
+      data["filter[servers]"] = filterServers;
+    }
+
+    if (filterSearch) {
+      data["filter[search]"] = filterSearch;
+    }
+
+    if (filterBmid) {
+      data["filter[players]"] = filterBmid;
+    }
+
+    return await this.helpers.makeRequest({
+      method: "GET",
+      path: "/activity",
+      params: new URLSearchParams(data as { [key: string]: string }),
+    });
+  }
+}
